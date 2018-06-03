dd51c96c651539c128f053ecf7e9f9fbfae4584e
Hassam Malik <hssmmalik@gmail.com>
2018-06-03 20:44:33 +0500

    google auth added and integrated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, :except => [:index]
   def index
+  	if user_signed_in?
+  		redirect_to welcome_home_path
+  	end
   end
 
   def home
-  	
+  	@user = current_user
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def resource_name
+:user
+end
+
+def resource
+@resource ||= User.new
+end
+
+def devise_mapping
+@devise_mapping ||= Devise.mappings[:user]
+end
+  
+  def text_sanitize(text)
+    text = text.gsub(/(?:\n\r?|\r\n?)/, "")
+end
+  
 end
